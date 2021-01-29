a7c80418ca01da25ec96ba5536edc8ff6965c8f1
Mujahid Khan <almujahid1100@gmail.com>
2021-01-29 10:22:30 +0600

    fixed the issue

--- app/src/main/java/com/app/ble_demo/MainActivity.java
+++ app/src/main/java/com/app/ble_demo/MainActivity.java
@@ -9,16 +9,18 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-import com.github.douglasjunior.bluetoothclassiclibrary.BluetoothConfiguration;
-import com.github.douglasjunior.bluetoothclassiclibrary.BluetoothService;
-import com.github.douglasjunior.bluetoothclassiclibrary.BluetoothStatus;
-import com.github.douglasjunior.bluetoothlowenergylibrary.BluetoothLeService;
+import com.ficat.easyble.BleDevice;
+import com.ficat.easyble.BleManager;
+import com.ficat.easyble.gatt.callback.BleConnectCallback;
+import com.ficat.easyble.gatt.callback.BleNotifyCallback;
+import com.ficat.easyble.scan.BleScanCallback;
 
 import java.util.UUID;
 
+
 public class MainActivity extends AppCompatActivity {
 
-    BluetoothService service = null;
+    BleManager bleManager;
     TextView deviceNameTxt;
     public static final int REQUEST_ENABLE_BT = 1;
     public BLEReceiver bleReceiver;
@@ -30,34 +32,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         bleReceiver = new BLEReceiver(this);
         initViews();
-       service =  BluetoothService.getDefaultInstance();
-       service.setOnEventCallback(new BluetoothService.OnBluetoothEventCallback() {
-           @Override
-           public void onDataRead(byte[] buffer, int length) {
-               String data = new String(buffer);
-               Log.e("DATA_COMING", data);
-           }
-
-           @Override
-           public void onStatusChange(BluetoothStatus status) {
-
-           }
-
-           @Override
-           public void onDeviceName(String deviceName) {
-                deviceNameTxt.setText(deviceName);
-           }
-
-           @Override
-           public void onToast(String message) {
-
-           }
-
-           @Override
-           public void onDataWrite(byte[] buffer) {
-
-           }
-       });
+        initBLE();
     }
 
     private void initViews(){
@@ -75,6 +50,17 @@ public class MainActivity extends AppCompatActivity {
         registerReceiver(bleReceiver, new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED));
     }
 
+    private void initBLE(){
+        BleManager.ConnectOptions connectOptions = BleManager.ConnectOptions
+                .newInstance()
+                .connectTimeout(12000);
+         bleManager = BleManager
+                .getInstance()
+                .setConnectionOptions(connectOptions)//like scan options
+                .setLog(true, "DEBUG_BLE")
+                .init(this.getApplication());
+    }
+
 
     @Override
     protected void onStop() {
@@ -83,31 +69,98 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startScan(View view) {
-        service.setOnScanCallback(new BluetoothService.OnBluetoothScanCallback() {
+        bleManager.startScan(new BleScanCallback() {
             @Override
-            public void onDeviceDiscovered(BluetoothDevice device, int rssi) {
-                if(device.getName() != null){
-                    if(device.getName().contains("BPM")){
-                        service.connect(device);
-                    }
-                }
+            public void onLeScan(BleDevice device, int rssi, byte[] scanRecord) {
+                String name = device.name;
+                String address = device.address;
+                Log.e("BLE_NAME", name);
+                Log.e("BLE_ADDRESS", address);
+                       // if(name.contains("BPM")){
+                            deviceNameTxt.setText(name);
+                            connect(device);
+                            bleManager.stopScan();
+                        //}
 
             }
+
             @Override
-            public void onStartScan() {
-                Log.e("D_S", "Started");
+            public void onStart(boolean startScanSuccess, String info) {
+                if (startScanSuccess) {
+                    //start scan successfully
+                } else {
+                    //fail to start scan, you can see details from 'info'
+                    String failReason = info;
+                    Log.e("BLE_INFO", info);
+                }
             }
+
             @Override
-            public void onStopScan() {
-                Log.e("D_S", "Stoped");
+            public void onFinish() {
+
             }
         });
-        service.startScan();
-
     }
 
-    public void stopScan() {
-        Utils.toast(this, "Scan again....");
+    private void connect(BleDevice device){
+        BleConnectCallback bleConnectCallback = new BleConnectCallback() {
+            @Override
+            public void onStart(boolean startConnectSuccess, String info, BleDevice device) {
+                if (startConnectSuccess) {
+                    //start to connect successfully
+                } else {
+                    //fail to start connection, see details from 'info'
+                    String failReason = info;
+                }
+            }
+
+            @Override
+            public void onFailure(int failCode, String info, BleDevice device) {
+                if(failCode == BleConnectCallback.FAIL_CONNECT_TIMEOUT){
+                    //connection timeout
+                }else{
+                    //connection fail due to other reasons
+                }
+                Log.e("BLE_FAILING_INFO", info);
+            }
+
+            @Override
+            public void onConnected(BleDevice device) {
+                //device is connected
+                bleManager.notify(device, convertFromInteger(0xFFF0).toString(),
+                        convertFromInteger(0xFFF4).toString(), new BleNotifyCallback() {
+                            @Override
+                            public void onCharacteristicChanged(byte[] data, BleDevice device) {
+                                    String dataString = new String(data);
+                                    Log.e("RESULT", dataString);
+                            }
+
+                            @Override
+                            public void onNotifySuccess(String notifySuccessUuid, BleDevice device) {
+                                    Log.e("BLE_NOTIFY", "SUCCESS");
+                            }
+
+                            @Override
+                            public void onFailure(int failCode, String info, BleDevice device) {
+                                Log.e("BLE_NOTIFY", info);
+                            }
+                        });
+            }
+
+            @Override
+            public void onDisconnected(String info, int status, BleDevice device) {
+
+            }
+        };
+        bleManager.connect(device, bleConnectCallback);
+    }
+
+
+    public UUID convertFromInteger(int i) {
+        final long MSB = 0x0000000000001000L;
+        final long LSB = 0x800000805f9b34fbL;
+        long value = i & 0xFFFFFFFF;
+        return new UUID(MSB | (value << 32), LSB);
     }
 
 }
